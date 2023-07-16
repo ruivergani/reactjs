6c9ce7bd58a7a2e89c09a4ddb54429636d0ca895
ruivergani <ruiverganineto@gmail.com>
2023-07-16 20:21:49 +0100

    success message created

--- react-hook-form-yup/src/App.jsx
+++ react-hook-form-yup/src/App.jsx
@@ -1,5 +1,6 @@
-/* eslint-disable no-unused-vars */
-// Import React Hook Form
+import {useState} from 'react';
+
+// Import React Hook Form = ajuda a nao precisar criar estados de input (consegue pegar os valores em um objeto)
 import { useForm } from 'react-hook-form';
 // Import Yup
 import { yupResolver } from '@hookform/resolvers/yup'; 
@@ -19,6 +20,8 @@ const schema = yup.object({
 }).required(); // Schema is required in the form
 
 export function App() {
+  const [isSuccess, setIsSuccess] = useState(false);
+
   // Methods to use React Hook Form
   const { register, handleSubmit, formState: { errors }} = useForm({
     resolver: yupResolver(schema) // React Hook Form conected with Schema
@@ -26,9 +29,9 @@ export function App() {
   // register = registrar os fields de acordo com schema
   // handleSubmit = fazer algo com o sucesso do formulario
   // formState = errors para resposta caso nao validar
-
   // onSubmit
   const onSubmit = (data) => {
+    setIsSuccess(true); // display Success image
     console.log(data);
     console.log('Form data was sent.');
   };
@@ -41,34 +44,41 @@ export function App() {
             type="text" 
             placeholder="Type your name"
             {...register("name")}
+            className={(errors.name) ? `${formStyles.inputError}` : ``} // if exists error add the class inputError to the input
           />
-          <span>{errors.name?.message}</span>
+          <span className={(errors.name) ? `${formStyles.labelError}` : ``}>{errors.name?.message}</span>
         </div>
         <div className={formStyles.formGroup}>
           <input 
             type="email" 
             placeholder="Type your e-mail"
             {...register("email")}
+            className={(errors.email) ? `${formStyles.inputError}` : ``}
           />
-          <span>{errors.email?.message}</span>
+          <span className={(errors.email) ? `${formStyles.labelError}` : ``}>{errors.email?.message}</span>
         </div>
         <div className={formStyles.formGroup}>
           <input 
             type="password" 
             placeholder="Type your password"
             {...register("password")}
+            className={(errors.password) ? `${formStyles.inputError}` : ``} 
           />
-          <span>{errors.password?.message}</span>
+          <span className={(errors.password) ? `${formStyles.labelError}` : ``}>{errors.password?.message}</span>
         </div>
         <div className={formStyles.formGroup}>
           <input 
             type="password" 
             placeholder="Confirm your password"
             {...register("password_confirmation")}
+            className={(errors.password_confirmation) ? `${formStyles.inputError}` : ``} 
           />
-          <span>{errors.password_confirmation?.message}</span>
+          <span className={(errors.password_confirmation) ? `${formStyles.labelError}` : ``}>{errors.password_confirmation?.message}</span>
         </div>
         <button type="submit" className={formStyles.formSubmitButton}>Send Form</button>
+        {
+          isSuccess && <p className={formStyles.sucessMessage}>Formulario sent sucessfully.</p>
+        }
       </form>
     </div>
   );
