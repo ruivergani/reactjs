7359ca2ab70daf3c9e28ab04df3f172a5812cebf
ruivergani <ruiverganineto@gmail.com>
2023-07-16 17:33:16 +0100

    changes folder

--- react-hook-form-yup/src/App.jsx
+++ react-hook-form-yup/src/App.jsx
@@ -1,7 +1,4 @@
-import { Router } from './Router';
-
 import { BrowserRouter } from 'react-router-dom';
-import { Navigation } from './components/Navigation';
 
 // Lugar onde todas as paginas sao renderizadas
 export function App() {
@@ -9,8 +6,6 @@ export function App() {
     // The BrowserRouter tem que envolver a aplicacao inteira
     // Navigation = coloca aqui para otimizar a performance (Render apenas 1 vez)
     <BrowserRouter>
-      <Navigation/> 
-      <Router/>
     </BrowserRouter>
   );
 }
